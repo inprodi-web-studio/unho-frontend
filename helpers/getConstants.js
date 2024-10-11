df1f7b70f63981bfc7cc3f3d06a86aa375f9932a
inprodi-web-studio <proyectos@inprodi.mx>
2024-10-10 21:06:36 -0500

    update constants

--- helpers/getConstants.js
+++ helpers/getConstants.js
@@ -1,6 +1,6 @@
 const getConstants = () => {
     return {
-        API_URL : "https://mayfly-intimate-polliwog.ngrok-free.app/api"
+        API_URL : "https://unho-api-w2pwh.ondigitalocean.app/api"
     };
 };
 
