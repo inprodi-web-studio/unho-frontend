fb291b6d0c8d5b2f62e772094bd3643a02b852c7
Inprodi Web Studio <proyectos@inprodi.mx>
2024-11-25 11:29:51 -0600

    fix buffer file

--- helpers/createBufferFile.js
+++ helpers/createBufferFile.js
@@ -1,5 +1,14 @@
+import { Readable } from "stream";
+
 const createBufferFile = (base64) => {
-  return Buffer.from(base64, 'base64');
+  Buffer.from(base64, 'base64');
+
+  const stream = new Readable();
+  stream._read = () => {};
+  stream.push(buffer);
+  stream.push(null);
+
+  return stream;
 };
 
 export default createBufferFile;
