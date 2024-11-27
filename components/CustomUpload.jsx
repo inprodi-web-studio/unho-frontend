d67656dd36f1d3083c3a0570aff1d384a9f87ac9
Inprodi Web Studio <proyectos@inprodi.mx>
2024-11-27 10:35:33 -0600

    custom upload

--- components/CustomUpload.jsx
+++ components/CustomUpload.jsx
@@ -1,55 +1,56 @@
-import React from "react";
+import React, { forwardRef, useImperativeHandle, useState } from "react";
 import registerComponent from "@plasmicapp/host/registerComponent";
 
 import { Upload } from 'antd';
 const { Dragger } = Upload;
 
-export const CustomUpload = ({
-    value,
-    onChange,
-}) => {
+export const CustomUpload = forwardRef(({ value = [], onChange }, ref) => {
+    const [files, setFiles] = useState(value);
+
+    useImperativeHandle(ref, () => ({
+        getFiles: () => files,
+    }));
+
     return (
         <Dragger
             multiple
-            fileList={value}
-            listType="picture-card"
+            fileList={files}
+            listType="picture"
             onChange={(info) => {
-                onChange(info.fileList);
+                setFiles(info.fileList);
+                onChange && onChange();
             }}
         >
             <p>Subir Archivos</p>
         </Dragger>
-    )
-};
+    );
+});
+
+CustomUpload.displayName = "CustomUpload";
 
 export const customUploadMeta = {
     name: "CustomUpload",
     displayName: "Custom Upload",
-    states : {
-        value : {
-            type : "writable",
-            variableType : "array",
-            valueProp : "value",
-            onChangeProp : "onChange",
-        },
-    },
     props: {
-        value : {
-            type : "array",
+        value: {
+            type: "array",
         },
         onChange : {
             type : "eventHandler",
             argTypes : [],
         },
     },
+    refActions: {
+        getFiles: {
+          description: "Get current files",
+          argTypes: []
+        }
+    },
     importPath: "inprodi-design-system",
     importName: "CustomUpload",
 };
 
-export function registerCustomUpload(
-    loader,
-    customCustomUploadMeta
-) {
+export function registerCustomUpload(loader, customCustomUploadMeta) {
     const doRegisterComponent = (...args) =>
         loader ? loader.registerComponent(...args) : registerComponent(...args);
     doRegisterComponent(CustomUpload, customCustomUploadMeta ?? customUploadMeta);
